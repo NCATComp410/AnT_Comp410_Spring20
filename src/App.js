65e54c10cd3ebb2956ca91e58284b986bf955820
Cheick Berthe <cheickmec@users.noreply.github.com>
2020-02-10 16:32:48 -0500

    rename fileuploader root file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import './App.css';
-import SimpleFileUploader from "./components/FileUploader";
+import React from "react";
+import "./App.css";
+import SimpleFileUploader from "./components/FileUploader/FileUploader";
 
 function App() {
   return (
     <div className="App">
-      <SimpleFileUploader/>
+      <SimpleFileUploader />
     </div>
   );
 }
