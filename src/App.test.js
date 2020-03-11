185ccd80789ce8697ad9c5c2fdf425cbd5f5910f
Cheick Berthe <cheickmec@users.noreply.github.com>
2020-03-11 02:00:11 -0400

    add App component tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,22 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import { shallow } from "enzyme";
 import App from "./App";
+import SimpleFileUploader from "./components/FileUploader/FileUploader";
 
-test("renders learn react link", () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Test `App` Component", () => {
+  test("has class name `App`", () => {
+    const { container } = render(<App />);
+    expect(container.firstChild).toHaveClass("App");
+  });
+
+  test("renders Aggie Pride Text", () => {
+    const { container } = render(<App />);
+    expect(container).toHaveTextContent("Aggie Pride-Cyber Pirates");
+  });
+
+  test("renders `SimpleFileUploader", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(SimpleFileUploader).exists()).toBeTruthy();
+  });
 });
